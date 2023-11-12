f1404b88bf1117f418a14ce1579606b4d4e60236
JavierLMB~ <javier.camilleri29@gmail.com>
2023-11-12 12:25:05 +0100

    "initial setup"

---
+++ src/app/men/page.tsx
@@ -0,0 +1,10 @@
+import PreviewCard from "@/components/preview-card/previewCard";
+import React from "react";
+
+export default function MenPage() {
+  return (
+    <div>
+      <PreviewCard />
+    </div>
+  );
+}
---
+++ src/components/preview-card/useData.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+export function useData() {
+  const [storeProducts, setStoreProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(function () {
+    const controller = new AbortController();
+
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        setError("");
+        const res = await fetch(`https://fakestoreapi.com/products`, {
+          signal: controller.signal,
+        });
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching the data");
+
+        const data = await res.json();
+
+        if (data.Response === "False") throw new Error("Data not found");
+
+        setStoreProducts(data);
+        setError("");
+      } catch (err: any) {
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+
+    return function () {
+      controller.abort();
+    };
+  }, []);
+
+  return { storeProducts, isLoading, error };
+}
