a56d3052b5fd5fc70106e5b9b66b1ed84a3bcbc6
JavierLMB <javier.camilleri29@gmail.com>
2023-11-13 18:45:23 +0100

    "deploy error fix"

--- registery.tsx
+++ registery.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { useServerInsertedHTML } from "next/navigation";
 import { ServerStyleSheet, StyleSheetManager } from "styled-components";
