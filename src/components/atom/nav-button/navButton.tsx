cdb291cb8608906075ffb92cd99a6f26a6c66c3e
JavierLMB~ <javier.camilleri29@gmail.com>
2023-11-20 15:54:12 +0100

    "nav button added"

--- src/components/atom/nav-button/navButton.tsx
+++ src/components/atom/nav-button/navButton.tsx
@@ -8,10 +8,10 @@ type NavButtonType = {
 export default function NavButton({ navModalOpen, onClick }: NavButtonType) {
   return (
     <StyledNavButtonContainer $navModalOpen={navModalOpen} onClick={onClick}>
-      <StyledLineSmall1 $width="60%" />
-      <StyledLineBig1 $width="100%" />
-      <StyledLineSmall2 $width="60%" />
-      <StyledLineBig2 $width="100%" />
+      <StyledLineSmall1 $width="60%" $navModalOpen={navModalOpen} />
+      <StyledLineBig1 $width="100%" $navModalOpen={navModalOpen} />
+      <StyledLineSmall2 $width="60%" $navModalOpen={navModalOpen} />
+      <StyledLineBig2 $width="100%" $navModalOpen={navModalOpen} />
     </StyledNavButtonContainer>
   );
 }
@@ -34,26 +34,47 @@ const StyledNavButtonContainer = styled.div<{
 
 const StyledLine = styled.div<{ $width: string }>`
   position: absolute;
-  background-color: ${({ theme }) => theme.colors.accentGoldLighter};
   height: 0.4rem;
   border-radius: 1rem;
+  transition: all 0.3s cubic-bezier(0.01, -0.02, 0.51, 1.6);
+  background-color: ${({ theme }) => theme.colors.accentGoldLighter};
   ${({ $width }) => css`
     width: ${$width};
   `};
 `;
 
-const StyledLineSmall1 = styled(StyledLine)`
-  top: 0rem;
+const StyledLineSmall1 = styled(StyledLine)<{
+  $navModalOpen: boolean;
+}>`
+  ${({ $navModalOpen }) => css`
+    right: ${$navModalOpen ? "-3rem" : "0rem"};
+    top: 0rem;
+  `};
 `;
 
-const StyledLineBig1 = styled(StyledLine)`
-  top: 0.8rem;
+const StyledLineBig1 = styled(StyledLine)<{
+  $navModalOpen: boolean;
+}>`
+  ${({ $navModalOpen }) => css`
+    top: ${$navModalOpen ? "1.1rem" : "0.8rem"};
+    transform: rotate(${$navModalOpen ? "120deg" : "0deg"});
+  `};
 `;
 
-const StyledLineSmall2 = styled(StyledLine)`
+const StyledLineSmall2 = styled(StyledLine)<{
+  $navModalOpen: boolean;
+}>`
   bottom: 0.8rem;
+  ${({ $navModalOpen }) => css`
+    right: ${$navModalOpen ? "-3rem" : "0rem"};
+  `};
 `;
 
-const StyledLineBig2 = styled(StyledLine)`
-  bottom: 0rem;
+const StyledLineBig2 = styled(StyledLine)<{
+  $navModalOpen: boolean;
+}>`
+  ${({ $navModalOpen }) => css`
+    bottom: ${$navModalOpen ? "1.1rem" : "0rem"};
+    transform: rotate(${$navModalOpen ? "-120deg" : "0deg"});
+  `};
 `;
