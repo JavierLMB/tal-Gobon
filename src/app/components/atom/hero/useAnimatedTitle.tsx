b4fad75746ef29970afa2d784e034bc9520474a8
JavierLMB~ <javier.camilleri29@gmail.com>
2023-11-28 07:50:40 +0100

    "inview testing"

--- src/app/components/atom/hero/useAnimatedTitle.tsx
+++ src/app/components/atom/hero/useAnimatedTitle.tsx
@@ -3,17 +3,12 @@ import styled, { css } from "styled-components";
 
 type AnimatedTitleProp = {
   title: string;
+  inView: boolean;
 };
 
-const AnimatedTitle = ({ title }: AnimatedTitleProp) => {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    setLoaded(true);
-  }, []);
-
+const AnimatedTitle = ({ title, inView }: AnimatedTitleProp) => {
   const animatedTitle = title.split("").map((char, index) => (
-    <AnimatedLetter key={index} $delay={index * 0.1} $loaded={loaded}>
+    <AnimatedLetter key={index} $delay={index * 0.1} $loaded={inView}>
       {char}
     </AnimatedLetter>
   ));
@@ -38,3 +33,43 @@ const AnimatedLetter = styled.span<{ $delay: number; $loaded: boolean }>`
 `;
 
 export default AnimatedTitle;
+// import React, { useEffect, useState } from "react";
+// import styled, { css } from "styled-components";
+
+// type AnimatedTitleProp = {
+//   title: string;
+// };
+
+// const AnimatedTitle = ({ title }: AnimatedTitleProp) => {
+//   const [loaded, setLoaded] = useState(false);
+
+//   useEffect(() => {
+//     setLoaded(true);
+//   }, []);
+
+//   const animatedTitle = title.split("").map((char, index) => (
+//     <AnimatedLetter key={index} $delay={index * 0.1} $loaded={loaded}>
+//       {char}
+//     </AnimatedLetter>
+//   ));
+
+//   return <AnimatedTitleContainer>{animatedTitle}</AnimatedTitleContainer>;
+// };
+
+// const AnimatedTitleContainer = styled.div`
+//   display: inline-block;
+//   overflow: hidden;
+// `;
+
+// const AnimatedLetter = styled.span<{ $delay: number; $loaded: boolean }>`
+//   display: inline-block;
+//   transition: opacity 0.5s ease, transform 0.5s ease;
+
+//   ${({ $delay, $loaded }) => css`
+//     transition-delay: ${$delay}s;
+//     opacity: ${$loaded ? "1" : 0};
+//     transform: translateY(${$loaded ? "0" : "1em"});
+//   `};
+// `;
+
+// export default AnimatedTitle;
