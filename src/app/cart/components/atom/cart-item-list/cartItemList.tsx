b8a91b5017dfc01f8697d639f1412e87e25a157c
JavierLMB~ <javier.camilleri29@gmail.com>
2023-12-02 22:43:56 +0100

    styling cart

--- src/app/cart/components/atom/cart-item-list/cartItemList.tsx
+++ src/app/cart/components/atom/cart-item-list/cartItemList.tsx
@@ -2,6 +2,7 @@
 import styled from "styled-components";
 import { useCart } from "react-use-cart";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 
 const CartitemList = () => {
   const { items } = useCart();
@@ -10,15 +11,34 @@ const CartitemList = () => {
 
   return (
     <StyledCartItemListContainer>
+      <StyledCartItem>
+        <div>Continue Browsing</div>
+        <div>Name?</div>
+        <div>Price</div>
+        <div>Total</div>
+        <div>Bin?</div>
+      </StyledCartItem>
       {items.map((item) => (
         <StyledCartItem key={item.id}>
           <div>
-            <div>{item.name}</div>
-            <div>{item.weight}</div>
+            <StyledImage
+              src={item.image}
+              alt={item.name}
+              width={570}
+              height={660}
+              priority
+            />
           </div>
-          <div>€ {item.price}</div>
-          <div>{item.quantity}</div>
+          <div>
+            <div>{item.name}</div>
+            <div>{item.weight}g</div>
+          </div>
+          <div>
+            <div>€ {item.price}</div>
+            <div>{item.quantity}</div>
+          </div>
           <div>€ {item.itemTotal}</div>
+          <div>Bin</div>
         </StyledCartItem>
       ))}
     </StyledCartItemListContainer>
@@ -32,10 +52,21 @@ const StyledCartItemListContainer = styled.div`
   flex-direction: column;
   margin-bottom: 1rem;
 `;
+
 const StyledCartItem = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin: 1rem 1rem 0rem 1rem;
   padding-bottom: 1rem;
   border-bottom: 1px solid ${({ theme }) => theme.colors.primaryLight};
+`;
+
+const StyledImage = styled(Image)`
+  display: block;
+  width: 5rem;
+  height: 5rem;
+  object-fit: cover;
+  border-radius: 0.5rem;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
 `;
