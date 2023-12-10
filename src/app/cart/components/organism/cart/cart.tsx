e7ba6329c27a785fdda6e445b3fc4878cb7d2196
JavierLMB~ <javier.camilleri29@gmail.com>
2023-12-10 10:50:43 +0100

    "loader + removed storyobok"

--- src/app/cart/components/organism/cart/cart.tsx
+++ src/app/cart/components/organism/cart/cart.tsx
@@ -2,15 +2,35 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import CartItemListContainer from "../../molecule/cart-item-list-container/cartItemListcontainer";
 import CheckOutModal from "../../atom/check-out-modal/checkOutModal";
+import CartLoader from "./cartLoader";
 
 export default function Cart() {
   const [checkOutModalOpen, setCheckOutModalOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        setLoading(false);
+      } catch (error) {}
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <StyledCartContainer>
-      <CartItemListContainer
-        onClick={() => setCheckOutModalOpen(!checkOutModalOpen)}
-      />
+      {loading ? (
+        <CartLoader />
+      ) : (
+        <CartItemListContainer
+          onClick={() => setCheckOutModalOpen(!checkOutModalOpen)}
+        />
+      )}
       <CheckOutModal
         onClick={() => setCheckOutModalOpen(!checkOutModalOpen)}
         checkOutModalOpen={checkOutModalOpen}
@@ -27,3 +47,32 @@ const StyledCartContainer = styled.div`
     min-height: 60rem;
   }
 `;
+// import { useState, useEffect } from "react";
+// import styled from "styled-components";
+// import CartItemListContainer from "../../molecule/cart-item-list-container/cartItemListcontainer";
+// import CheckOutModal from "../../atom/check-out-modal/checkOutModal";
+
+// export default function Cart() {
+//   const [checkOutModalOpen, setCheckOutModalOpen] = useState(false);
+
+//   return (
+//     <StyledCartContainer>
+//       <CartItemListContainer
+//         onClick={() => setCheckOutModalOpen(!checkOutModalOpen)}
+//       />
+//       <CheckOutModal
+//         onClick={() => setCheckOutModalOpen(!checkOutModalOpen)}
+//         checkOutModalOpen={checkOutModalOpen}
+//       />
+//     </StyledCartContainer>
+//   );
+// }
+
+// const StyledCartContainer = styled.div`
+//   display: grid;
+//   place-content: center;
+//   min-height: 40rem;
+//   @media (min-width: ${({ theme }) => theme.breakpoints.bpNormals}) {
+//     min-height: 60rem;
+//   }
+// `;
---
+++ src/app/cart/components/organism/cart/cartLoader.tsx
@@ -0,0 +1,69 @@
+import styled, { keyframes } from "styled-components";
+import { BiCheese } from "react-icons/bi";
+
+export default function CartLoader() {
+  return (
+    <StyledLoaderContainer>
+      <StyledCheeseContainer>
+        <StyledCheeseIcon />
+      </StyledCheeseContainer>
+      <StyledDotsContainer>
+        <StyledDot delay="0s" />
+        <StyledDot delay="0.3s" />
+        <StyledDot delay="0.6s" />
+      </StyledDotsContainer>
+    </StyledLoaderContainer>
+  );
+}
+
+const StyledLoaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const StyledCheeseContainer = styled.div`
+  padding: 2rem;
+  border-radius: 50%;
+  animation: ${spin} 2s infinite;
+  background: linear-gradient(
+    45deg,
+    ${({ theme }) => theme.colors.primaryDark},
+    ${({ theme }) => theme.colors.primaryLight}
+  );
+`;
+
+const StyledCheeseIcon = styled(BiCheese)`
+  transform: scale(-1, 1);
+  font-size: clamp(5rem, 15vw, 7rem);
+  color: ${({ theme }) => theme.colors.accentGoldLighter};
+`;
+
+const dotFade = keyframes`
+  0%, 80%, 100% {
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
+`;
+
+const StyledDotsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledDot = styled.div<{ delay: string }>`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin: 0 4px;
+  animation: ${dotFade} 2s infinite ${({ delay }) => delay || "0s"};
+  background-color: ${({ theme }) => theme.colors.primaryDark};
+`;
